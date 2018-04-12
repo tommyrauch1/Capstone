491baef66e89e49467a9cb918a956eb4d70b3f81
Tommy Rauch <tommy.rauch2014@gmail.com>
2018-04-12 16:11:27 -0500

    Fixed constant rotation
    Mostly done, at least using IR sensor. Will branch to try with ultrasonic

--- IRSensor.py
+++ IRSensor.py
@@ -2,20 +2,34 @@
 from ev3dev.ev3 import *
 from threading import Thread
 
-def Scan(dir):
+def Scan():
+	global objectSpotted
 	ir = InfraredSensor()
 	eyeMotor = LargeMotor('outC')
 	objectSpotted = False
-	while(!objectSpotted):
-		eyeMotor.run_to_rel_pos(position_sp=dir, speed_sp=100, stop_action="brake")
+	dir = 175
+	while(objectSpotted == False):
+		eyeMotor.stop()
+		eyeMotor.run_to_rel_pos(position_sp=dir, speed_sp=75, stop_action="brake")
 		distance = ir.value()
-		if(ir.value < 100):
+		if(ir.value() < 30):
 			objectSpotted = True
-	return objectSpotted
+			eyeMotor.stop()
+			print("spotted")
+		dir *= -1
+		eyeMotor.wait_while('running')
 
-def Slap(dir):
+def Slap():
 	slapMotor = LargeMotor('outA')
-	slapMotor.run_to_rel_pos(position_sp=dir, speed_sp=1000, stop_action="brake")
+	slapMotor.run_to_rel_pos(position_sp=180, speed_sp=1000, stop_action="hold")
+	slapMotor.wait_while('running')
+	slapMotor.run_to_rel_pos(position_sp=-180, speed_sp=1000, stop_action="hold")
 
-scanThread = Thread(target=Scan)
-slapThread = Thread(target=Slap)
+#scanThread = Thread(target=Scan)
+#slapThread = Thread(target=Slap)
+#scanThread.start()
+global objectSpotted
+objectSpotted = False
+while(objectSpotted == False) :
+	Scan()
+Slap()
